021642b6f3d404880ac64beddead5e98890d41cf
molmardo <molnarmark2005@gmail.com>
2025-04-24 11:13:46 +0200

    Navbar fix

--- include/navigationbar.php
+++ include/navigationbar.php
@@ -1,10 +1,14 @@
 <?php error_reporting(0);?>
+<?php
+$isHomepage = basename($_SERVER['SCRIPT_NAME']) === 'index.php';
+?>
+
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="stylesheet" href="css/navigationbar.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"> 
-<nav class="navbar">
+<nav class="navbar <?php echo $isHomepage ? 'navbar-index' : 'navbar-default'; ?>">
     <div class="content">
       <div class="logo">
         <a href="./">
