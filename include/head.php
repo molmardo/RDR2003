8fd556f096b420ded8ced141f0d631623069ec9a
molmardo <molnarmark2005@gmail.com>
2025-04-25 18:55:18 +0200

    head tags

--- include/head.php
+++ include/head.php
@@ -6,7 +6,15 @@
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/6416005539.js" crossorigin="anonymous"></script>
-
+    <!-- Robots Meta Tags -->
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
     <!-- Favicon -->
-
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <!-- Open Graph Meta Tags for Social Media -->
+    <!-- Twitter Card Meta Tags -->
+    <!-- Additional SEO Meta Tags -->
+    <meta name="language" content="Hungarian">
+    <meta name="revisit-after" content="7 days">
+    <meta name="theme-color" content="#fe9901">
+    <!-- Canonical URL -->
