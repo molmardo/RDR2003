c84abf68bafcc393d8fb9e1be986a7328e14ac14
molmardo <molnarmark2005@gmail.com>
2025-06-20 16:11:21 +0200

    meta tags

--- include/head.php
+++ include/head.php
@@ -138,21 +138,33 @@ $fullUrl = $protocol . $host . $uri;
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/6416005539.js" crossorigin="anonymous"></script>
  <!-- Essential Meta Tags -->
+  <meta name="description" content="<?= $lang['metatags']['description'] ?>">
+  <meta name="keywords" content="<?= $lang['metatags']['keywords'] ?>">
+  <meta name="author" content="RDR2003">
 <!-- Robots Meta Tags -->
 <meta name="robots" content="index, follow">
 <meta name="googlebot" content="index, follow">
 <!-- Favicon -->
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 <!-- Open Graph Meta Tags for Social Media -->
+ <meta property="og:type" content="website">
+<meta property="og:title" content="<?= $lang['metatags']['og-title'] ?>">
+<meta property="og:description" content="<?= $lang['metatags']['og-description'] ?>">
+<meta property="og:url" content="<?= BASE_URL . $_SERVER['REQUEST_URI'] ?>">
+<meta property="og:site_name" content="RDR 2003 Kft">
+<meta property="og:image" content="<?= BASE_URL .'/imgs/social-preview.jpg' ?>">
 <!-- Twitter Card Meta Tags -->
+ <meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="<?= $lang['metatags']['og-title'] ?>">
+<meta name="twitter:description" content="<?= $lang['metatags']['og-description'] ?>">
+<meta name="twitter:image" content="<?= BASE_URL .'/imgs/social-preview.jpg' ?>">
+<meta name="twitter:site" content="@rdr2003">
 <!-- Additional SEO Meta Tags -->
 <meta name="language" content="Hungarian">
 <meta name="revisit-after" content="7 days">
-<!-- <meta name="theme-color" content="#fe9901"> -->
 <meta name="theme-color" content="#000">
 <!-- Canonical URL -->
 <link rel="canonical" href="<?=$host?>">
 
-<!--  -->
 
 <link rel="alternate" href="<?= $fullUrl ?>" hreflang="<?= $currentLang ?>" />
